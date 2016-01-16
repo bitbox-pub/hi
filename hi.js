da5a3d328d43792278b5e1c75c24d13c59b46018
Sergiu Toderascu <sergiu.toderascu@gmail.com>
2016-01-16 16:12:47 +0200

    Update hi.js

--- hi.js
+++ hi.js
@@ -1,10 +1,19 @@
+import { bit, box, pub } from 'bitbox'
+import { h1, input } from 'bitbox/base'
+
 <hi(props, name) export>
-	<h1 => 'Hello ', this.name || String(name)
-	<input on-input=(e => this.pub('name', e.target.value)) />
+	<h1 => 'Hello ', this.name
+	<input on-input=(e => pub(this, 'name', e.target.value)) />
 </hi>
 
-hi.props = {}
+hi.bit = { name: 'World' }
 
-bitbox({ name: 'World' }, hi)
+let node = document.body
+
+export const app = new bit(hi, function(i) {
+	node = box(node, hi.call(this))
+})
+
+pub(app, 'name', 'Scooby Doo')
 
 /** hi!box */
